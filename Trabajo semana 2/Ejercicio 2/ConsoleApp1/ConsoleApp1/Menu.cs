bf487ca6d79c4f63c93cd90aed2994e36404a425
Hollow4U <131100806+Hollow4U@users.noreply.github.com>
2024-03-31 19:56:36 -0500

    Se agregan armas a una lista

--- Trabajo semana 2/Ejercicio 2/ConsoleApp1/ConsoleApp1/Menu.cs
+++ Trabajo semana 2/Ejercicio 2/ConsoleApp1/ConsoleApp1/Menu.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
     class Menu
     {
         string name;
-        List<Armas> listArmas = new List<Armas>();
+        List<Weapons> listArmas = new List<Weapons>();
 
         public void Execute()
         {
@@ -33,6 +33,7 @@ namespace ConsoleApp1
                         AddArmas();
                         break;
                     case 2:
+                        EditWeapons();
                         break;
                     case 3:
                         continueFlag = false;
@@ -43,52 +44,131 @@ namespace ConsoleApp1
                 }
 
             }
-
-        }
-
-                for (int i = 0; i > listArmas.Count; i++)
+            void AddArmas()
             {
-            
+                Console.WriteLine("¿Que arma quieres comprar?:");
+                Console.WriteLine("1. Espada");
+                Console.WriteLine("2. Arco");
+                Console.WriteLine("3. Pistola");
+
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        GetSword();
+                        break;
+                    case 2:
+                        GetBow();
+                        break;
+                    case 3:
+                        GetPistol();
+                        break;
+                    default:
+                        Console.WriteLine("No existe la opcion");
+                        break;
+                }
             }
 
-        public void AddArmas()
-        {
-
-            Console.WriteLine("¿Que arma quieres comprar?:");
-            Console.WriteLine("1. Espada");
-            Console.WriteLine("2. Arco");
-            Console.WriteLine("3. Pistola");
-
-            int option = int.Parse(Console.ReadLine());
-
-            switch (option)
+            Sword GetSword()
             {
-                case 1:
-                    
-                    Console.WriteLine("Introducir nombre del arma:");
-                     = Console.ReadLine();
+                Console.WriteLine("Introducir el daño del arma:");
+                float DMG = float.Parse(Console.ReadLine());
+                Console.WriteLine("Introducir la velocidad de ataque del arma:");
+                float DPS = float.Parse(Console.ReadLine());
+                Console.WriteLine("Introducir el precio deseado:");
+                float PRICE = float.Parse(Console.ReadLine());
+                if(PRICE > money)
+                {
+                    Console.WriteLine("No tienes suficiente dinero para comprar esta arma");
+                    return null;
+                }
+                else
+                {
+                    money = money - PRICE;
+                    Console.WriteLine("Nombra tu arma:");
+                    string NICK = Console.ReadLine();
+                    Console.WriteLine($"Te quedan {money} para comprar");
 
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
+                    listArmas.Add(new Sword(NICK, DMG, DPS, PRICE));
+                    return new Sword(NICK, DMG, DPS, PRICE);
+                }
             }
 
+            Bow GetBow()
+            {
+                Console.WriteLine("Introducir el daño del arma:");
+                float DMG = float.Parse(Console.ReadLine());
+                Console.WriteLine("Introducir la velocidad de ataque del arma:");
+                float DPS = float.Parse(Console.ReadLine());
+                Console.WriteLine("Introducir el precio deseado:");
+                float PRICE = float.Parse(Console.ReadLine());
+                if (PRICE > money)
+                {
+                    Console.WriteLine("No tienes suficiente dinero para comprar esta arma");
+                    return null;
+                }
+                else
+                {
+                    money = money - PRICE;
+                    Console.WriteLine("Nombra tu arma:");
+                    string NICK = Console.ReadLine();
+                    Console.WriteLine($"Te quedan {money} para comprar");
 
+                    listArmas.Add(new Bow(NICK, DMG, DPS, PRICE));
+                    return new Bow(NICK, DMG, DPS, PRICE);
+                }
+            }
+
+            Pistol GetPistol()
+            {
+                Console.WriteLine("Introducir el daño del arma:");
+                float DMG = float.Parse(Console.ReadLine());
+                Console.WriteLine("Introducir la velocidad de ataque del arma:");
+                float DPS = float.Parse(Console.ReadLine());
+                Console.WriteLine("Introducir el precio deseado:");
+                float PRICE = float.Parse(Console.ReadLine());
+                if (PRICE > money)
+                {
+                    Console.WriteLine("No tienes suficiente dinero para comprar esta arma");
+                    return null;
+                }
+                else
+                {
+                    money = money - PRICE;
+                    Console.WriteLine("Nombra tu arma:");
+                    string NICK = Console.ReadLine();
+                    Console.WriteLine($"Te quedan {money} para comprar");
+
+                    listArmas.Add(new Pistol(NICK, DMG, DPS, PRICE));
+                    return new Pistol(NICK, DMG, DPS, PRICE);
+                }
+            }
+
+            void EditWeapons()
+            {
+                Console.WriteLine("¿Que operacion quieres realizar?:");
+                Console.WriteLine("1. Ver las armas disponibles");
+                Console.WriteLine("2. Vender arma");
+
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        foreach(Weapons Nick in listArmas)
+                        {
+                            Console.WriteLine($"{Nick.ToString()}");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("No existe la opcion");
+                        break;
+                    default:
+                        Console.WriteLine("No existe la opcion");
+                        break;
+                }
+            }
         }
-
-        private Sword GetSword()
-        {
-            Console.WriteLine("Introducir el daño del arma:");
-            float DMG = float.Parse(Console.ReadLine());
-            Console.WriteLine("Introducir la velocidad de ataque del arma:");
-            float DPS = float.Parse(Console.ReadLine());
-            Console.WriteLine("Introducir el precio deseado:");
-            float PRICE = float.Parse(Console.ReadLine());
-
-            return new Sword(DMG,DPS,PRICE);
-        }
-
     }
 }
